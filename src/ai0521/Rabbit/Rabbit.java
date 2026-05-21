c7936d18be709c63701b4353fbb53fd684a4dca9
jodongju12 <2601110302@gspace.kopo.ac.kr>
2026-05-21 11:39:09 +0900

    Java 6장 for문 완료

---
+++ src/ai0521/Rabbit/Rabbit.java
@@ -0,0 +1,13 @@
+package ai0521.Rabbit;
+
+public class Rabbit {
+    String shape;  // 토끼의 모양
+    int xPos;      // X 위치
+    int yPos;      // Y 위치
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
