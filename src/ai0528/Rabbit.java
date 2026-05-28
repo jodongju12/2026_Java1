67f33a20152f0c110b43bf6c2a85831c2b8c1bb6
jodongju12 <2601110302@gspace.kopo.ac.kr>
2026-05-28 11:47:53 +0900

    Java 7장 클래스와 객체 접근제한자

---
+++ src/ai0528/Rabbit.java
@@ -0,0 +1,28 @@
+package ai0528;
+
+public class Rabbit {
+
+//  private String shape;
+//  private int x;
+//  private int y;
+
+
+
+
+
+
+
+
+
+    String shape;
+    int x;
+    int y;
+
+
+
+
+
+//    public void setLocation(int x, int y){
+//      this.shape = shape;
+    }
+}
---
+++ src/ai0528/RunRabbit.java
@@ -0,0 +1,18 @@
+package ai0528;
+
+
+public class RunRabbit {
+    public static void main(String[] args) {
+        RunRabbit rabbit = new Rabbit();
+
+
+
+//        rabbit.setShape("동그라미");
+        rabbit.shape = "삼각형";
+        rabbit.printInfo();
+//        rabbit.setLacation(50, 80);
+        rabbit.x = 70;
+        rabbit.y = 100;
+        rabbit.printInfo();
+    }
+}
