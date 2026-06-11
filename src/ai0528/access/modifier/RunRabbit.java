037849ac1be4e0687a505052e28e9632142fd890
jodongju12 <2601110302@gspace.kopo.ac.kr>
2026-06-11 11:46:30 +0900

    Java 8장 추상클래스, 인터페이스

--- src/ai0528/access/modifier/RunRabbit.java
+++ src/ai0528/access/modifier/RunRabbit.java
@@ -1,16 +1,13 @@
-package ai0528;
-
+package ai0528.access.modifier;
 
 public class RunRabbit {
     public static void main(String[] args) {
-        RunRabbit rabbit = new rabbit();
-
-
+        Rabbit rabbit = new Rabbit();
 
 //        rabbit.setShape("동그라미");
         rabbit.shape = "삼각형";
         rabbit.printInfo();
-//        rabbit.setLacation(50, 80);
+//        rabbit.setLocation(50, 80);
         rabbit.x = 70;
         rabbit.y = 100;
         rabbit.printInfo();
